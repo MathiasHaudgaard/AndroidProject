a8652ec3a648d871006804785a1ad6dadfe5328e
Rasmus Thorsøe <rt071195@gmail.com>
2015-09-12 14:59:34 +0200

    Add Class to handle network requests

---
+++ app/src/main/java/NetworkSingleton.java
@@ -0,0 +1,44 @@
+import android.app.Application;
+import android.text.TextUtils;
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Creates a single RequestQueue for the entire app to use.
+ */
+public class NetworkSingleton {
+
+    private static NetworkSingleton mInstance;
+    private RequestQueue mRequestQueue;
+    private Context mCtx; //ApplicationContext
+
+    private NetworkSingleton(Context context) {
+        mCtx = context.getApplicationContext();
+        mRequestQueue = getRequestQueue();
+    }
+
+    //Should be called with Application context to create singleton
+    public static NetworkSingleton getInstance(Context context) {
+        if (mInstance == null)
+            mInstance = new NetworkSingleton(context);
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null)
+            mRequestQueue = Volley.newRequestQueue(mCtx); //Creates RequestQueue with ApplicationContext
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> request) {
+        getRequestQueue().add(request);
+    }
+
+    public void cancelAllRequests(Object tag) {
+        if (mRequestQueue != null)
+            mRequestQueue.cancelAll(tag);
+    }
+}
