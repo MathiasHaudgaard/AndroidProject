ee8531680b93ac000517a8775d2156cdcd5cddd4
Rasmus Thorsøe <rt071195@gmail.com>
2015-09-12 18:59:46 +0200

    Add Appconfig file containing server ip

---
+++ app/src/main/java/com/example/mathias/helloworld/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.mathias.helloworld;
+
+/**
+ * Created by rasmustrew on 9/12/15.
+ */
+public class AppConfig {
+
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.101/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.101/android_login_api/";
+}
