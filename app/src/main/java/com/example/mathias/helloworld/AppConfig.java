9016f61061ee5de1a5d50553861263235a3951ba
Rasmus Thorsøe <rt071195@gmail.com>
2015-09-13 10:57:02 +0200

    Update URL to server

--- app/src/main/java/com/example/mathias/helloworld/AppConfig.java
+++ app/src/main/java/com/example/mathias/helloworld/AppConfig.java
@@ -6,8 +6,8 @@ package com.example.mathias.helloworld;
 public class AppConfig {
 
     // Server user login url
-    public static String URL_LOGIN = "http://hjem.andersholm.eu:32500";
+    public static String URL_LOGIN = "http://hjem.andersholm.eu:32500/android_login_api/";
 
     // Server user register url
-    public static String URL_REGISTER = "http://hjem.andersholm.eu:32500";
+    public static String URL_REGISTER = "http://hjem.andersholm.eu:32500/android_login_api/";
 }
