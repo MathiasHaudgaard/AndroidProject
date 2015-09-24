d6791cd2c4f2574ed6b593124959959e82454058
Rasmus Thorsøe <rt071195@gmail.com>
2015-09-24 14:04:17 +0200

    Add update of user position

--- app/src/main/java/com/example/mathias/helloworld/MapsActivity.java
+++ app/src/main/java/com/example/mathias/helloworld/MapsActivity.java
@@ -2,13 +2,21 @@ package com.example.mathias.helloworld;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,9 +24,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private LocationManager mLocationManager; // Provides information on user location
+    private boolean mRequestingUpdates = false;
+    private double mLatitude;
+    private double mLongitude;
+    private int minUpdateTime = 0;
+    private int minUpdateDist = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,28 +82,24 @@ public class MapsActivity extends FragmentActivity {
     }
 
     /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
+     * This is where we can add markers or lines, add listeners or move the camera.
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker").snippet("Snippet"));
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker").snippet("Snippet"));
+        //Dummy marker?
 
         // Enable MyLocation Layer of Google Map
-        mMap.setMyLocationEnabled(true);
-
-        // Get LocationManager object from System Service LOCATION_SERVICE
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //mMap.setMyLocationEnabled(true);
 
         // Create a criteria object to retrieve provider
         Criteria criteria = new Criteria();
 
         // Get the name of the best provider
-        String provider = locationManager.getBestProvider(criteria, true);
+        String provider = mLocationManager.getBestProvider(criteria, true);
 
-        // Get Current Location
-        Location myLocation = locationManager.getLastKnownLocation(provider);
+        // Get Current Location from the best provider
+        Location myLocation = mLocationManager.getLastKnownLocation(provider);
         //Fejlen ovenover angiver om vi har faaet adgang til last know location.
         //Vi kan godt indfoere tjekket eller droppe det
         //Den beholder vi simpelthen bare og dropper at checke efter noget.
@@ -91,27 +107,118 @@ public class MapsActivity extends FragmentActivity {
         // set map type
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        // Get latitude of the current location
-        double latitude = myLocation.getLatitude();
-
-        // Get longitude of the current location
-        double longitude = myLocation.getLongitude();
+        // Get latitude and longitude of the current location
+        mLatitude = myLocation.getLatitude();
+        mLongitude = myLocation.getLongitude();
 
         // Create a LatLng object for the current location
-        LatLng latLng = new LatLng(latitude, longitude);
+        LatLng myCoordinates = new LatLng(mLatitude, mLongitude);
 
         // Show the current location in Google Map
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myCoordinates));
 
         // Zoom in the Google Map
         mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Dig").snippet("Her er jeg!"));
+        
+        // Add marker showing your location
+        mMap.addMarker(new MarkerOptions().position(new LatLng(mLatitude, mLongitude)).title("Dig").snippet("Her er jeg!"));
 
-        //Function for zooming to current location
-        LatLng myCoordinates = new LatLng(latitude, longitude);
-        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(myCoordinates, 12);
-        mMap.animateCamera(yourLocation);
+        // Function for zooming to current location
+        //CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(myCoordinates, 12);
+        //mMap.animateCamera(yourLocation);
+
+        //Request continous updates
+        requestUpdatesIfNeeded(provider);
+
+
     }
 
 
+    private LocationManager getLocationManager() {
+        if (mLocationManager == null)
+            // Get our LocationManager object from System Service LOCATION_SERVICE
+            mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return mLocationManager;
+    }
+
+    //Request continous updates
+    private void requestUpdatesIfNeeded(String provider){
+
+        if (!mRequestingUpdates) {
+            // create locationListener, will receive location updates
+            LocationListener locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    mLatitude = location.getLatitude();
+                    mLongitude = location.getLongitude();
+                    updateServerPosition();
+
+                }
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {}
+                @Override
+                public void onProviderEnabled(String s) {}
+                @Override
+                public void onProviderDisabled(String s) {}
+            };
+
+            //start receiving continous position updates
+            mLocationManager.requestLocationUpdates(provider, minUpdateTime, minUpdateDist, locationListener);
+
+            mRequestingUpdates = true;
+        }
+
+    }
+
+    private boolean updateServerPosition() {
+        String req_tag = "req_update_position";
+
+        StringRequest req = new StringRequest(Request.Method.POST,
+                AppConfig.URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override  //If succesfull, should move to next screen
+                    public void onResponse(String response) {
+                        Log.d("position update", "position update response: " + response);
+
+                        try {
+                            //Create JSONObject, easier to work with
+                            JSONObject JResponse = new JSONObject(response);
+                            boolean error = JResponse.getBoolean("error");
+                            if (!error) {}
+                            //Shows an error if we couldn't login and prints the error message from server
+                            //TODO don't show the direct message but write a custom error message
+                            else{
+                                Log.e("position update", "position update error: " + response);
+                                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG)
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override //If not succesfull, show user error message
+            //only does it, if there's a network error, not login error
+            public void onErrorResponse(VolleyError error) {
+                Log.e("login", "Login error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG)
+                        .show();
+            }
+        })  {
+            @Override // Set all parameters for for server
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("tag", "updatePosition");
+                params.put("email", UserStatic.getEmail());
+                params.put("latitude", String.valueOf(mLatitude));
+                params.put("longitude", String.valueOf(mLongitude));
+                return params;
+            }
+        };
+
+        req.addMarker(req_tag);
+        //Adding request to request queue
+        NetworkSingleton.getInstance(getApplicationContext()).addToRequestQueue(req);
+        return true;
+    }
 }
