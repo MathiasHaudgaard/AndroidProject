3456162d5f2e89d6d01e663abbeaaa9d9895cc7e
Rasmus Thorsøe <rt071195@gmail.com>
2015-10-01 14:00:05 +0200

    add semicolon

--- app/src/main/java/com/example/mathias/helloworld/HomeActivity/PagesFragment.java
+++ app/src/main/java/com/example/mathias/helloworld/HomeActivity/PagesFragment.java
@@ -21,7 +21,7 @@ public class PagesFragment extends Fragment {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
             }
         }
-    }
+    };
 
 
 	public PagesFragment(){}
