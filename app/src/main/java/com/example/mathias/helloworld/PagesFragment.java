26b9f2624d1d7dc982f5c124e6b226c71e4dd188
Mathias Haudgaard <mathiashaudgaard@gmail.com>
2015-10-01 23:20:18 +0200

    Fix broadcastReceiver and create ArrayAdapter including ListView. Everything works.

--- app/src/main/java/com/example/mathias/helloworld/PagesFragment.java
+++ app/src/main/java/com/example/mathias/helloworld/PagesFragment.java
@@ -1,25 +1,38 @@
 package com.example.mathias.helloworld;
 
 import android.app.Fragment;
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PagesFragment extends Fragment {
-	private final BroadcastReceiver mReciever = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if(BluetoothDevice.ACTION_FOUND.equals(action)){
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-            }
-        }
-    }
+
+
+    private ListView listView;
+    private ArrayList<String> item;
+    private ArrayAdapter<String> mArrayAdapter;
+    private BroadcastReceiver mReceiver;
+
+
+
+    // Create a BroadcastReceiver for ACTION_FOUND
+
+
+    // Register the BroadcastReceiver
+    /*IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+    registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy*/
+
 
 
 	public PagesFragment(){}
@@ -27,9 +40,43 @@ public class PagesFragment extends Fragment {
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
- 
         View rootView = inflater.inflate(R.layout.fragment_pages, container, false);
+        item = new ArrayList<>();
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter.startDiscovery();
+
+        // Create a BroadcastReceiver for ACTION_FOUND
+        mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                // When discovery finds a device
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    // Get the BluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    // Add the name and address to an array adapter to show in a ListView
+                    mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            }
+        };
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        getActivity().registerReceiver(mReceiver, filter);
+
+
+        mArrayAdapter = new ArrayAdapter<>(this.getActivity(), android.R.layout.simple_list_item_1, item);
+        listView = (ListView) rootView.findViewById(R.id.listView);
+        listView.setAdapter(mArrayAdapter);
+ 
+
          
         return rootView;
     }
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        getActivity().unregisterReceiver(mReceiver);
+    }
+
+
+
 }
