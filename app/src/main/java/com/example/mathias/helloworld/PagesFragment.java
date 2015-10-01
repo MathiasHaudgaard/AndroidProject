895c5b80aee294b3d0bfe341c52b247904fa4737
Martin H <night5142@gmail.com>
2015-10-01 13:34:04 +0200

    Create a broadcarst reciever to track and discover new bluetoothsignals / devices

--- app/src/main/java/com/example/mathias/helloworld/PagesFragment.java
+++ app/src/main/java/com/example/mathias/helloworld/PagesFragment.java
@@ -1,13 +1,27 @@
 package com.example.mathias.helloworld;
 
 import android.app.Fragment;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class PagesFragment extends Fragment {
-	
+	private final BroadcastReceiver mReciever = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            }
+        }
+    }
+
+
 	public PagesFragment(){}
 	
 	@Override
