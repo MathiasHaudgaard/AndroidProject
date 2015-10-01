96b4a7bb8c1031250536e8ec3cc77a208cdc58a1
Martin H <night5142@gmail.com>
2015-10-01 12:04:36 +0200

    Bluetooth

--- app/src/main/java/com/example/mathias/helloworld/MainActivity.java
+++ app/src/main/java/com/example/mathias/helloworld/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.mathias.helloworld;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
