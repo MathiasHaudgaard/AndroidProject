8d6f136058737d02acdb7e2e7d014aa900755352
Anders Holm <Holm@d03409.local>
2015-10-08 16:15:56 +0200

    added highscore fragment and removed Whats Hot fragment

--- app/src/main/java/com/example/mathias/helloworld/Highscore.java
+++ app/src/main/java/com/example/mathias/helloworld/Highscore.java
@@ -11,12 +11,10 @@ public class Highscore extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View mainView = inflater.inflate(R.layout.fragment_whats_hot, container, false);
+        View mainView = inflater.inflate(R.layout.fragment_highscore, container, false);
 
-        WebView mywebView = (WebView) mainView.findViewById(R.id.webView);  //ERROR 1
-        //mywebView.getSettings().setJavaScriptEnabled(true);  //ERROR 2
-        //mywebView.setWebViewClient(new SwAWebClient());  //ERROR 3
-        mywebView.loadUrl("http://hjem.andersholm.eu:32500/android_login_api/treasures.php");  //ERROR 4
+        WebView mywebView = (WebView) mainView.findViewById(R.id.webView); // Laver et webView
+        mywebView.loadUrl("http://hjem.andersholm.eu:32500/android_login_api/treasures.php"); // Henter hjemmesiden med HighScore
 
         return mainView;
     }
